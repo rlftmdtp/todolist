bcab359d919bcc060a357ece93c281f03e44d2ea
길승세 <rlftmdtp@hanmail.net>
2017-06-04 01:43:28 +0900

    첫 테스트

--- server/src/main/java/kr/or/connect/todo/TodoApplication.java
+++ server/src/main/java/kr/or/connect/todo/TodoApplication.java
@@ -1,12 +1,31 @@
 package kr.or.connect.todo;
 
+import java.util.Collections;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import kr.or.connect.todo.persistence.TodoDao;
 
 @SpringBootApplication
 public class TodoApplication extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+		
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		DataSource dataSource = context.getBean(DataSource.class);
+		TodoDao dao = new TodoDao(dataSource);
+		int count = dao.countBooks();
+		System.out.println(count);
+		context.close();
 	}
 }
--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -1,5 +1,8 @@
 package kr.or.connect.todo.persistence;
 
+import java.util.Collections;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,10 +14,20 @@ public class TodoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
 	
+	// todo로 변경
+	private static final String COUNT_BOOK = "SELECT COUNT(*) FROM book";
+	
 	public TodoDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		/*
 		this.insertAction = new SimpleJdbcInsert(dataSource)
 				.withTableName("todo")
 				.usingGeneratedKeyColumns("id");
+				*/
+		}
+	
+	public int countBooks() {
+		Map<String, Object> params = Collections.emptyMap();
+		return jdbc.queryForObject(COUNT_BOOK, params, Integer.class);
 	}
 }
